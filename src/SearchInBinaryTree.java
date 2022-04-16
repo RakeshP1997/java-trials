8e0f043489cbf9d34651e406d1ce939e1ebd595e
Rakesh Phalak <rakeshphalak703@gmail.com>
2022-04-16 13:09:56 +0530

    adding print logic for output

--- src/SearchInBinaryTree.java
+++ src/SearchInBinaryTree.java
@@ -36,6 +36,26 @@ public class SearchInBinaryTree {
         }
     }
 
+    public static void printInOrder(TreeNode root) {
+        if (root == null) {
+            System.out.print("null ");
+            return;
+        }
+        printInOrder(root.left);
+        System.out.print(root.val + " ");
+        printInOrder(root.right);
+    }
+
+    public static void printPreOrder(TreeNode root) {
+        if (root == null) {
+            System.out.print("null ");
+            return;
+        }
+        System.out.print(root.val + " ");
+        printInOrder(root.left);
+        printInOrder(root.right);
+    }
+
     public static void main(String[] args) {
         long beforeUsedMem = Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
         long startTime = System.nanoTime();
@@ -46,7 +66,12 @@ public class SearchInBinaryTree {
         TreeNode root = new TreeNode(4,left,right);
 
         TreeNode output = new Solution().searchBST(root, 2);
-        System.out.println("Output: " + output != null ? output.val : null);
+        System.out.println("Output: ");
+        System.out.print("InOrder: ");
+        printInOrder(output);
+        System.out.println();
+        System.out.print("PreOrder: ");
+        printPreOrder(output);
         long endTime = System.nanoTime();
         long afterUsedMem = Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
         System.out.println("\nTime Taken:" + (endTime - startTime)/1000000.0);
