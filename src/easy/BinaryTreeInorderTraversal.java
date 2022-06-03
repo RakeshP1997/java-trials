8128d3ba22e32eca5004abae75c47b3901e6374d
Rakesh Phalak <rakeshphalak703@gmail.com>
2022-06-03 11:45:10 +0530

    added access modifiers

--- src/easy/BinaryTreeInorderTraversal.java
+++ src/easy/BinaryTreeInorderTraversal.java
@@ -10,7 +10,7 @@ import java.util.List;
  *
  */
 public class BinaryTreeInorderTraversal {
-    public class TreeNode {
+    private class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
